4a6649c83c1ddb86b90b57de312cc6d521670614
Nico Garbaccio <garbaccio20@gmail.com>
2024-02-02 16:11:04 -0500

    fix close sign in modal bug

--- src/components/CloseModal.tsx
+++ src/components/CloseModal.tsx
@@ -1,20 +1,34 @@
 "use client";
 
 import { X } from "lucide-react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { FC } from "react";
 import { Button } from "./ui/Button";
 
 interface CloseModalProps {}
 
 const CloseModal: FC<CloseModalProps> = ({}) => {
-  const router = useRouter();
+  //   const router = useRouter();
+
+  const handleCloseModal = () => {
+    const previousUrl = document.referrer;
+
+    if (
+      previousUrl &&
+      !previousUrl.includes("/sign-in") &&
+      !previousUrl.includes("/sign-up")
+    ) {
+      window.location.href = previousUrl;
+    } else {
+      window.location.href = "/";
+    }
+  };
 
   return (
     <Button
       variant="subtle"
       className="h-6 w-6 p-0 rounded-md"
-      onClick={() => router.back()}
+      onClick={handleCloseModal}
     >
       <X aria-label="close modal" className="h-4 w-4" />
     </Button>
