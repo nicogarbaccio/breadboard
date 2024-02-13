1d8b258a22bf8ae88f7c33f487f1e89fe4a8bcda
Nico Garbaccio <garbaccio20@gmail.com>
2024-02-13 13:37:45 -0500

    custom feeds for logged in & logged out users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,36 @@
+import CustomFeed from "@/components/homepage/CustomFeed";
+import GeneralFeed from "@/components/homepage/GeneralFeed";
 import { buttonVariants } from "@/components/ui/Button";
-import { HomeIcon } from "lucide-react";
+import { getAuthSession } from "@/lib/auth";
+import { Home as HomeIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function Home() {
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export default async function Home() {
+  const session = await getAuthSession();
+
   return (
     <>
-      <h1 className="font-bold text-3xl med:text-4xl">Your feed</h1>
+      <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-        {/* feed */}
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
 
         {/* subreddit info */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
           <div className="bg-emerald-100 px-6 py-4">
             <p className="font-semibold py-3 flex items-center gap-1.5">
-              <HomeIcon className="w-4 h-4" />
+              <HomeIcon className="h-4 w-4" />
               Home
             </p>
           </div>
-
-          <div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+          <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
             <div className="flex justify-between gap-x-4 py-3">
               <p className="text-zinc-500">
-                Your personal Breadboard homepage. Come here to check in with
-                your favorite food communities.
+                Your personal Breadit frontpage. Come here to check in with your
+                favorite communities.
               </p>
             </div>
 
@@ -30,11 +38,11 @@ export default function Home() {
               className={buttonVariants({
                 className: "w-full mt-4 mb-6",
               })}
-              href="/r/create"
+              href={`/r/create`}
             >
               Create Community
             </Link>
-          </div>
+          </dl>
         </div>
       </div>
     </>
